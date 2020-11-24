28f65eff834278aa122b267cedacfc14fc0f1ee0
WaspGeekBrains <wasporc@mail.ru>
2020-11-25 00:50:45 +0300

    проверка пароля и смена имени

---
+++ src/ru/geekbrains/java3/User.java
@@ -0,0 +1,32 @@
+package ru.geekbrains.java3;
+//Entity
+public class User {
+    //Id
+    private int id;
+    private String name;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
